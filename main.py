7b27e25b0a128d6e5180b6098e35ce4c8396dae3
Dmitry Permyakov <taren4ik@mail.ru>
2025-11-03 00:39:10 +1000

    add upload_to_s3

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
+import json
 import boto3
 import dotenv
 import requests
+from botocore.exceptions import NoCredentialsError, ClientError
 
 dotenv.load_dotenv()
 access_token = os.getenv("ACCESS_TOKEN")
+access_key_id = os.getenv("KEY_ID")
+secret_access_key = os.getenv("ACCESS_KEY")
 
 url = "https://api.wordstat.yandex.net/v1/dynamics"
 
@@ -15,7 +19,7 @@ headers = {
     "Authorization": f"Bearer {access_token}"
 }
 year = datetime.now().year
-
+date_now = datetime.now().date().strftime("_%Y_%m_%d")
 
 data = {
     "phrase": "мос ру",
@@ -42,22 +46,39 @@ def get_response(**kwargs):
     return response.json()
 
 
-def save_s3(data):
+def upload_s3(data):
     """
-    Parse response.
-    :param weather:
+    Upload to s3 file.
+    :param data:
     :return:
     """
+    try:
+        s3 = boto3.client(
+            's3',
+            endpoint_url='http://127.0.0.1:9100',
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            region_name='us-east-1'
+        )
+        buffer = json.dumps(data, ensure_ascii=False).encode("utf-8")
+        s3.put_object(
+            Body = buffer,
+            Bucket = "wordstat",
+            Key = f"mos_ru_{date_now}.json",
+        )
+        print("✅ Upload successful")
 
-    json_data = data
-    pfrase = data['requestPhrase']
-    dynamics = data['dynamics']
-    # minioClient = Minio('minio:9000',
-    #                     access_key='weak_access_key',
-    #                     secret_key='weak_secret_key',
-    #                     secure=False)
-    # minioClient.list_buckets()
+    except FileNotFoundError:
+        print("❌ File not found")
+    except NoCredentialsError:
+        print("❌ Invalid credentials")
+    except ClientError as e:
+        print(f"❌ AWS error: {e}")
+
+    # json_data = data
+    # pfrase = data['requestPhrase']
+    # dynamics = data['dynamics']
 
 
 if __name__ == '__main__':
-    save_s3(get_response())
+    upload_s3(get_response())
